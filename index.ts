55db85d292b6aee2fb452f771d42c2a7c781df60
Madhusuthanan-B <madhub081011@gmail.com>
2019-11-13 23:57:52 +0530

    New data set added without time property

--- index.ts
+++ index.ts
@@ -10,7 +10,11 @@ class TimeSeries {
 let data1 = [{ time: 1, data: '60' }, { time: 2, data: '30' }, { time: 3, data: '20' }];
 let data2 = [{ time: 5, value: 20 }, { time: 6, value: 25 }];
 
+// New data set without time property
+let data3 = [{ t: 10, value: 50 }, { t: 11, value: 60 }];
+
 var series = new TimeSeries();
 
 console.log(series.getTimes(data1));
 console.log(series.getTimes(data2));
+console.log(series.getTimes(data3));
