e5e0fda0477f050260d3512845e1dea828fc0889
Madhusuthanan-B <madhub081011@gmail.com>
2019-11-14 00:17:11 +0530

    Adding interface constraint and making method as generic

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+interface ITime {
+  time: number;
+}
+
 class TimeSeries {
 
-    getTimes(arg: any) {
+    getTimes<T>(arg: T[]) {
         return {
             times: arg.map((x: any) => x.time)
         };
