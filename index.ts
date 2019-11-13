574a5f55a405924dc53e65b2d56de8a24f50f352
Madhusuthanan-B <madhub081011@gmail.com>
2019-11-13 23:46:25 +0530

    Keeping the sample data outside

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 class TimeSeries {
-    data1 = [{ time: 1, data: '60' }, { time: 2, data: '30' }, { time: 3, data: '20' }];
-    data2 = [{ time: 5, value: 20 }, { time: 6, value: 25 }];
 
     getTimes(arg: any) {
         return {
@@ -8,3 +6,11 @@ class TimeSeries {
         };
     }
 }
+
+let data1 = [{ time: 1, data: '60' }, { time: 2, data: '30' }, { time: 3, data: '20' }];
+let data2 = [{ time: 5, value: 20 }, { time: 6, value: 25 }];
+
+var series = new TimeSeries();
+
+console.log(series.getTimes(data1));
+console.log(series.getTimes(data2));
