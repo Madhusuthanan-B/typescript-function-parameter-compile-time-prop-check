934ec2ccc9986fb048a10b6476f2c0f559bf7982
Madhusuthanan-B <madhub081011@gmail.com>
2019-11-14 00:30:41 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -11,11 +11,11 @@ class TimeSeries {
     }
 }
 
-let data1 = [{ time: 1, data: '60' }, { time: 2, data: '30' }, { time: 3, data: '20' }];
-let data2 = [{ time: 5, value: 20 }, { time: 6, value: 25 }];
+const data1 = [{ time: 1, data: '60' }, { time: 2, data: '30' }, { time: 3, data: '20' }];
+const data2 = [{ time: 5, value: 20 }, { time: 6, value: 25 }];
 
 // New data set without time property
-let data3 = [{ t: 10, value: 50 }, { t: 11, value: 60 }];
+const data3 = [{ t: 10, value: 50 }, { t: 11, value: 60 }];
 
 var series = new TimeSeries();
 
